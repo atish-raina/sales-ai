e4eaefc0f42795abe4b8892ed72138af607c6cb9
atish <araina.c@roshn.sa>
2026-09-07 16:08:31 +0530

    Add prompt chaining

--- index.js
+++ index.js
@@ -10,41 +10,57 @@ const rl = readline.createInterface({
 	output: process .stdout,
 });
 
+let previousResponseId = null;
 
-rl.question("user: ", async(input) => {
-	try {
-		const response = await client.responses.create({
-			model: "gpt-5.6",
-			input: "what is a docker image",
-			instructions: `
-				You are a Sales AI assistant.
+function prompt() {
+	rl.question("user: ", async(input) => {
+		if(input.trim().toLowerCase() === "exit") {
+			rl.close();
+			return;
+		}
 
-				You may ONLY answer questions related to:
-				- sales
-				- lead generation
-				- prospecting
-				- cold outreach
-				- sales emails
-				- sales calls
-				- objections
-				- qualification
-				- CRM
-				- sales pipelines
-				- account management
-				- closing deals
-				- sales strategy
+		try {
+			const response = await client.responses.create({
+				model: "gpt-5.6",
+				...(previousResponseId && {
+      				previous_response_id: previousResponseId,
+    			}),
+				instructions: `
+					You are a Sales AI assistant.
 
-				If the user asks anything outside sales, reply exactly:
+					You may ONLY answer questions related to:
+					- sales
+					- lead generation
+					- prospecting
+					- cold outreach
+					- sales emails
+					- sales calls
+					- objections
+					- qualification
+					- CRM
+					- sales pipelines
+					- account management
+					- closing deals
+					- sales strategy
 
-				"I can only help with sales-related questions."
+					If the user asks anything outside sales, reply exactly:
 
-				Do not answer general knowledge, coding, politics, entertainment,
-				personal advice, medical, legal, or other non-sales questions.`,
-				
-				input: input,
-			});
-		}	
-	})
+					"I can only help with sales-related questions."
+
+					Do not answer general knowledge, coding, politics, entertainment,
+					personal advice, medical, legal, or other non-sales questions.`,
+					
+					input: input,
+				});
+				previousResponseId = response.id;
+				console.log("\nLLm:",response.output_text, "\n");
+			} catch (error) {
+				console.log(error.message);
+			}
+			prompt();
+	});
+}
+prompt();
 
 
-console.log(response.output_text);
+
