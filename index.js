43180389d4acb746224fb33faac97347dd257675
atish <araina.c@roshn.sa>
2026-09-07 21:51:41 +0530

    Add configs on tools

--- index.js
+++ index.js
@@ -5,10 +5,20 @@ const client = new OpenAI({
 	apiKey: process.env.OpenAI_API_KEY,
 });
 
+const tools = JSON.parse(
+	fs.readFileSync("./tools.json", "utf8")
+	);
+
+const config = JSON.parse(
+	fs.readFileSync("./config.json", "utf8")
+);
+
 const rl = readline.createInterface({
 	input: process.stdin, 
 	output: process .stdout,
 });
+
+
 
 let previousResponseId = null;
 
@@ -25,32 +35,9 @@ function prompt() {
 				...(previousResponseId && {
       				previous_response_id: previousResponseId,
     			}),
-				instructions: `
-					You are a Sales AI assistant.
-
-					You may ONLY answer questions related to:
-					- sales
-					- lead generation
-					- prospecting
-					- cold outreach
-					- sales emails
-					- sales calls
-					- objections
-					- qualification
-					- CRM
-					- sales pipelines
-					- account management
-					- closing deals
-					- sales strategy
-
-					If the user asks anything outside sales, reply exactly:
-
-					"I can only help with sales-related questions."
-
-					Do not answer general knowledge, coding, politics, entertainment,
-					personal advice, medical, legal, or other non-sales questions.`,
-					
-					input: input,
+				instructions: config.instructions,
+				input,
+				tools,
 				});
 				previousResponseId = response.id;
 				console.log("\nLLm:",response.output_text, "\n");
